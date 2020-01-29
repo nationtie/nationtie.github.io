3a7b28c34fcf530d0140e5cdde83092845eef88f
Nationtie <57921105+nationtie@users.noreply.github.com>
2020-01-29 14:12:24 +0800

    New website updated

--- theme/site.js
+++ theme/site.js
@@ -4,9 +4,7 @@ function showFull(path){
 	}
 	var layer = '<div id="show-image" onclick="closeFull()"></div>';
 	$('body').prepend(layer);
-	var layer1 = '<div style="height: 10%;"></div>';
-	$('#show-image').append(layer1);
-	var image = '<img src="'+ path + '" />';
+	var image = '<img src="'+ path + '">';
 	$('#show-image').append(image);
 	$(document).scroll(closeFull);
 }
